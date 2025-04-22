bb29d603bd4b7235130a0536ef310f49c57513c1
João Vitor de Souza <jvitorsouza345@gmail.com>
2025-04-22 20:07:03 -0300

    Update teste.c

--- teste.c
+++ teste.c
@@ -4,7 +4,8 @@
 void testar(){
   //função de teste simples
   assert(1 == 1); //verifica se 1 é igual a 1
-  printf("Teste passou!\n"); 
+  printf("Teste passou!\n");
+  printf("Assim não po rra!\n");
 }
 
 int main() {
